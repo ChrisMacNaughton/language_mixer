2a797a3fd9ce4a49711a33a2866d0f96242f425d
Chris MacNaughton <chmacnaughton@gmail.com>
2015-01-25 21:38:42 -0500

    test shared stuff

--- spec/translator/translator_spec.rb
+++ spec/translator/translator_spec.rb
@@ -1,4 +1,26 @@
 require 'spec_helper'
-describe LanguageMixer::Translator do
-  pending "Test Shared Functionality"
+describe LanguageMixer::Translator::Translate do
+  before :each do
+    @client = LanguageMixer::Translator::Translate.new
+    @client.target_language = 'es'
+    @client.source_language = 'en'
+  end
+
+  it "knows it's source language" do
+    expect(@client.source_language).to eq('en')
+  end
+
+  it "knows it's target language" do
+    expect(@client.target_language).to eq('es')
+  end
+
+  it 'can change target language' do
+    @client.target_language = 'it'
+    expect(@client.target_language).to eq('it')
+  end
+
+  it 'can change source language' do
+    @client.source_language = 'it'
+    expect(@client.source_language).to eq('it')
+  end
 end
