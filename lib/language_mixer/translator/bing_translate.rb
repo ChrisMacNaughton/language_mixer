4aa8a5b33f8307d4eda5e526e4ef85cd0ab8db45
Chris MacNaughton <chmacnaughton@gmail.com>
2015-01-25 20:50:20 -0500

    bing -> microsoft only

--- lib/language_mixer/translator/bing_translate.rb
+++
@@ -1,5 +0,0 @@
-module LanguageMixer::Translator
-  class BingTranslate
-    
-  end
-end
--- spec/translator/bing_translate_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'spec_helper'
-describe LanguageMixer::Translator::BingTranslate do
-  pending "Test Bing API"
-end
